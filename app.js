ab5f2c64552886b6984e746a62a87fcf4f70f0a1
TechWithAyaan <techwithayaanfaheem@gmail.com>
2025-10-02 07:38:25 -0700

    main

---
+++ app.js
@@ -0,0 +1,11 @@
+function openeye(){
+    document.getElementById("password").type = "password"
+    document.getElementsByClassName("o") [0].style.display = "none"
+    document.getElementsByClassName("n") [0].style.display = "inline"
+
+}
+function closeye(){
+        document.getElementById("password").type = "text"
+    document.getElementsByClassName("n") [0].style.display = "none"
+document.getElementsByClassName("o")[0].style.display = "inline"
+}
